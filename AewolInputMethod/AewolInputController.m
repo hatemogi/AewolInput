f3804ad029e761572558db4b715bf39fe6d44257
Daehyun Kim <hatemogi@gmail.com>
2014-09-03 18:26:23 +0900

    fn키로 쿼티 입력할 수 있게 기능추가

--- AewolInputMethod/AewolInputController.m
+++ AewolInputMethod/AewolInputController.m
@@ -33,46 +33,11 @@
     }
 }
 
-#define AEWOL_QWERTY       "asdfhgzxcv\tbqweryt123465=97-80]ou[ip\tlj'k;\\,/nm.\t `"
-#define AEWOL_QWERTY_SHIFT "ASDFHGZXCV\1BQWERYT!@#$^%+(&_*)}OU{IP\tLJ\"K:|<?NM>\t ~"
-#define AEWOL_DVORAK       ""
-#define AEWOL_DVORAK_SHIFT ""
-
-- (BOOL)inputText:(NSString*)string key:(NSInteger)keyCode modifiers:(NSUInteger)flags client:(id)sender
+- (BOOL)updateBothCommitAndPreedit:(BOOL)handled client:(id)sender
 {
-    static char *keymaps[] = {AEWOL_QWERTY, AEWOL_QWERTY_SHIFT};
-    static int MAX_KEYCODE = strlen(AEWOL_QWERTY) - 1;
-
-    NSLog(@"keycode = %ld (%ld) for = %@", keyCode, flags, string);
-    if ((flags | NSShiftKeyMask) != NSShiftKeyMask || keyCode > MAX_KEYCODE) {
-        [self flushPreedit:sender];
-        return NO;
-    }
-
-    BOOL handled = NO;
-    
-    if (keyCode == 51) {
-        handled = hangul_ic_backspace(ctx);
-    } else {
-        int shift = ((flags & NSShiftKeyMask) > 0) ? 1 : 0;
-        char ascii = keymaps[shift][keyCode];
-        if (isalpha(ascii)) {
-            handled = hangul_ic_process(ctx, ascii);
-        } else {
-            [self flushPreedit:sender];
-            if (ascii == '\t') {
-                return NO;
-            } else {
-                NSString *asciiString = [NSString stringWithFormat:@"%c", ascii];
-                [sender insertText:asciiString replacementRange:NSMakeRange(NSNotFound, NSNotFound)];
-                return YES;
-            }
-        }
-    }
-    
     const ucschar *commit = hangul_ic_get_commit_string(ctx);
     const ucschar *preedit = hangul_ic_get_preedit_string(ctx);
-
+    
     if (handled) {
         if (commit[0] != 0) {
             NSString *c = [self stringFromUCS4:commit];
@@ -81,9 +46,60 @@
         NSString *p = [self stringFromUCS4:preedit];
         [sender setMarkedText:p selectionRange:NSMakeRange(0, p.length) replacementRange:NSMakeRange(NSNotFound, NSNotFound)];
     }
+    
     return handled;
 }
 
+#define AEWOL_QWERTY       "asdfhgzxcv\tbqweryt123465=97-80]ou[ip\tlj'k;\\,/nm.\b `"
+#define AEWOL_QWERTY_SHIFT "ASDFHGZXCV\1BQWERYT!@#$^%+(&_*)}OU{IP\tLJ\"K:|<?NM>\b ~"
+#define AEWOL_DVORAK       ""
+#define AEWOL_DVORAK_SHIFT ""
+
+- (BOOL)inputText:(NSString*)string key:(NSInteger)keyCode modifiers:(NSUInteger)flags client:(id)sender
+{
+    static char *keymaps[] = {AEWOL_QWERTY, AEWOL_QWERTY_SHIFT};
+    static int MAX_KEYCODE = strlen(AEWOL_QWERTY) - 1;
+
+    // fn키 누르면, NSNumericPadKeyMask가 켜져서 온다.
+    NSUInteger FN_KEY_MASK = NSFunctionKeyMask | NSNumericPadKeyMask;
+    NSUInteger fn_shift_mask = NSShiftKeyMask | FN_KEY_MASK;
+    BOOL fn_or_shift = (flags | fn_shift_mask) == fn_shift_mask;
+
+    if (!fn_or_shift || keyCode > MAX_KEYCODE) {
+        [self flushPreedit:sender];
+        return NO;
+    }
+
+    int shift = ((flags & NSShiftKeyMask) > 0) ? 1 : 0;
+    char ascii = keymaps[shift][keyCode];
+    
+    if (ascii == '\b') {
+        if (flags & FN_KEY_MASK) {
+            // fn backspace는 delete 키로 처리됨.
+            [self flushPreedit:sender];
+            return NO;
+        } else {
+            return [self updateBothCommitAndPreedit:hangul_ic_backspace(ctx) client:sender];
+        }
+    } else {
+        NSString *ascii_s = [NSString stringWithFormat:@"%c", ascii];
+    
+        if (!isalpha(ascii) || (flags & NSFunctionKeyMask) > 0) {
+            // 알파벳이 아니거나, fn 조합으로 누를 경우에는 qwerty 입력처리하자.
+            [self flushPreedit:sender];
+            if (ascii == '\t') {
+                return NO;
+            } else {
+                NSLog(@"inserting %@ for %c (%ld)", ascii_s, ascii, keyCode);
+                [sender insertText:ascii_s replacementRange:NSMakeRange(NSNotFound, NSNotFound)];
+                return YES;
+            }
+        } else {
+            // 알파벳이고 fn이 안 눌린 상태면 여기로 온다.
+            return [self updateBothCommitAndPreedit:hangul_ic_process(ctx, ascii) client:sender];
+        }
+    }
+}
 
 -(void)setValue:(id)value forTag:(long)tag client:(id)sender
 {
